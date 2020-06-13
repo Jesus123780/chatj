e7d99b9fa46769d3c51b10c98b5826c55991e5ee
Jesus123780 <53837840+Jesus123780@users.noreply.github.com>
2020-06-12 23:46:21 -0500

    Updates

---
+++ precache-manifest.0d5b01d1d70bd142e6fcb3420aa85dd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d3695a8c825896efdee2949ef09dc7",
+    "url": "/chatj/index.html"
+  },
+  {
+    "revision": "bfb42a0ec2ea8e2ac534",
+    "url": "/chatj/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "3bb104c3dd9b21400447",
+    "url": "/chatj/static/js/2.bc322344.chunk.js"
+  },
+  {
+    "revision": "bfb42a0ec2ea8e2ac534",
+    "url": "/chatj/static/js/main.9e003b53.chunk.js"
+  },
+  {
+    "revision": "7748f43d6b81bad61392",
+    "url": "/chatj/static/js/runtime~main.7a150762.js"
+  }
+]);
